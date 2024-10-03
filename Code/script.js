608cc333b814c1d7bc597dd54a2f7eba9e93481c
Devin Wijesiri <wijesiri2001@gmail.com>
2024-10-02 23:04:18 -0400

    completed

--- Code/script.js
+++ Code/script.js
@@ -8,11 +8,31 @@ function addTask(){
     }
     else
     {
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
+        const li = document.createElement("li");
+        li.textContent = inputBox.value;
+
+        const timestamp = new Date();
+        const options = {year:'numeric', month:'long',day:'numeric'};
+        const formattedTime = timestamp.toLocaleDateString('en-US', options);
+
+        const  timeSpan = document.createElement("span");
+        timeSpan.textContent = ` - ${formattedTime}`;
+        timeSpan.style.fontSize = "14px";
+        timeSpan.style.color = "#888";
+        timeSpan.style.marginLeft = "10px";
+        li.appendChild(timeSpan);
+
         listContainer.appendChild(li);
+
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
+        span.style.position = "absolute" ;
+        span.style.right = "0";
+        span.style.top = "5px";
+        span.style.width = "40px"; 
+        span.style.height = "40px";
+        span.style.fontSize = "22px";
+        span.style.borderRadius = "50%";
         li.appendChild(span);
     }
     inputBox.value = "";
